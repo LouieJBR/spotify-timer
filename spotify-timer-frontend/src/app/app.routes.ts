7b1391cbf819c1bb07ba1b738d561ecd2d0c2a13
Louie Brown <78971075+LouieJBR@users.noreply.github.com>
2024-10-05 14:43:43 +0100

    animated bubbles background

--- spotify-timer-frontend/src/app/app.routes.ts
+++ spotify-timer-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },         // Route to LoginComponent
+    { path: 'login', component: LoginComponent }, 
+    { path: '', component: LandingComponent },         // Route to LoginComponent
   ];
